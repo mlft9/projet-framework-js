6cd7fb970886ee438984dcd97d151d332caffa02
Maxime <maxime.lefort@etik.com>
2025-10-03 11:44:27 +0200

    add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./App.css"
+import "./App.css";
 
 function App() {
   const [titre, setTitre] = useState("");
@@ -24,55 +24,46 @@ function App() {
     <div>
       <h1>Bonjour 👋</h1>
       <h2>Voici un formulaire de to-do list</h2>
-      <div
-        style={{
-          backgroundColor: "#343434ff",
-          padding: "20px",
-          borderRadius: "8px",
-          width: "250px",
-        }}
-      >
-        <form
-          action="#"
-          style={{ display: "flex", flexDirection: "column", width: "200px" }}
-        >
-          <p>Titre de la tache</p>
-          <input
-            type="text"
-            placeholder="Titre"
-            value={titre}
-            onChange={(e) => setTitre(e.target.value)}
-            required
-          />{" "}
-          <br />
-          <p>Description de la tache</p>
-          <input
-            type="text"
-            placeholder="Description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />{" "}
-          <br />
-          <p>Date d'échéance</p>
-          <input
-            type="date"
-            value={dateEcheance}
-            onChange={(e) => setDateEcheance(e.target.value)}
-          />{" "}
-          <br />
-          <button onClick={addTache}>Ajouter</button>
-        </form>
-      </div>
-      <div>
-        <h2>Liste des tâches</h2>
-        <div>
-          {liste.map((tache, index) => (
-            <div key={index} style={{ marginBottom: "10px" }}>
-              <h3>{tache.titre}</h3>
-              <p>{tache.description}</p>
-              <p>Date d'échéance: {tache.dateEcheance}</p>
-            </div>
-          ))}
+      <div className="main-layout">
+        <div className="form-col">
+          <form action="#">
+            <p>Titre de la tache</p>
+            <input
+              type="text"
+              placeholder="Titre"
+              value={titre}
+              onChange={(e) => setTitre(e.target.value)}
+              required
+            />
+            <p>Description de la tache</p>
+            <input
+              type="text"
+              placeholder="Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <p>Date d'échéance</p>
+            <input
+              type="date"
+              value={dateEcheance}
+              onChange={(e) => setDateEcheance(e.target.value)}
+            />
+            {titre && (
+              <button onClick={addTache}>Ajouter</button>
+            )}
+          </form>
+        </div>
+        <div className="taches-list">
+          <h2>Liste des tâches</h2>
+          <ul>
+            {liste.map((tache, index) => (
+              <li className="tache-item" key={index}>
+                <h3>{tache.titre}</h3>
+                <p>Description : {tache.description}</p>
+                <p className="date">Date d'échéance : {tache.dateEcheance}</p>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
